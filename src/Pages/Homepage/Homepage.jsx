35489ea4347c428183becabf240c515af0125a80
SajeedAninda <sajaninda@gmail.com>
2024-01-06 23:39:58 +0600

    Created How We Work Timeline

--- src/Pages/Homepage/Homepage.jsx
+++ src/Pages/Homepage/Homepage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from '../../Components/Banner/Banner';
 import WhatWeDo from '../../Components/What We Do/WhatWeDo';
 import HowWeHelp from '../../Components/How We Help/HowWeHelp';
+import HowWeWork from '../../Components/How We Work/HowWeWork';
 
 const Homepage = () => {
     return (
@@ -9,6 +10,7 @@ const Homepage = () => {
             <Banner></Banner>
             <WhatWeDo></WhatWeDo>
             <HowWeHelp></HowWeHelp>
+            <HowWeWork></HowWeWork>
         </div>
     );
 };
