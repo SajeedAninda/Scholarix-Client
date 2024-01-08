67bb5749cc3e3d4e9145fa42462d29212688b702
SajeedAninda <sajaninda@gmail.com>
2024-01-08 15:30:30 +0600

    Added Counter to success section

--- src/Components/Our Success/OurSuccess.jsx
+++ src/Components/Our Success/OurSuccess.jsx
@@ -4,8 +4,14 @@ import { IoIosPeople } from "react-icons/io";
 import { BsGlobeCentralSouthAsia } from "react-icons/bs";
 import { FaUniversity } from "react-icons/fa";
 import "./success.css"
+import CountUp from 'react-countup';
 
 const OurSuccess = () => {
+    let AnimatedCounter = ({ end, duration }) => (
+        <CountUp end={end} duration={duration} separator="," />
+    );
+
+
     return (
         <div className='successBg mt-16 py-16'>
             <div className='mx-auto w-[90%]'>
@@ -19,26 +25,34 @@ const OurSuccess = () => {
 
                 <div className='grid grid-cols-4 gap-6 text-white'>
                     <div className='flex flex-col items-center space-y-3'>
-                        <BsFillPeopleFill className='text-[100px]'/>
-                        <h3 className='text-4xl font-bold'>2200+</h3>
+                        <BsFillPeopleFill className='text-[100px]' />
+                        <h3 className='text-4xl font-bold'>
+                            <AnimatedCounter end={2200} duration={3} /> +
+                        </h3>
                         <h3 className='text-3xl font-bold'>Satisfied Clients</h3>
                     </div>
 
                     <div className='flex flex-col items-center space-y-3'>
-                        <BsGlobeCentralSouthAsia className='text-[100px]'/>
-                        <h3 className='text-4xl font-bold'>9</h3>
+                        <BsGlobeCentralSouthAsia className='text-[100px]' />
+                        <h3 className='text-4xl font-bold'>
+                            <AnimatedCounter end={9} duration={3} />
+                        </h3>
                         <h3 className='text-3xl font-bold'>Countries</h3>
                     </div>
 
                     <div className='flex flex-col items-center space-y-3'>
-                        <FaUniversity className='text-[100px]'/>
-                        <h3 className='text-4xl font-bold'>100+</h3>
+                        <FaUniversity className='text-[100px]' />
+                        <h3 className='text-4xl font-bold'>
+                            <AnimatedCounter end={100} duration={3} /> +
+                        </h3>
                         <h3 className='text-3xl font-bold'>Universities</h3>
                     </div>
 
                     <div className='flex flex-col items-center space-y-3'>
-                        <IoIosPeople className='text-[100px]'/>
-                        <h3 className='text-4xl font-bold'>700+</h3>
+                        <IoIosPeople className='text-[100px]' />
+                        <h3 className='text-4xl font-bold'>
+                            <AnimatedCounter end={700} duration={3} /> +
+                        </h3>
                         <h3 className='text-3xl font-bold'>Students Helped</h3>
                     </div>
                 </div>
