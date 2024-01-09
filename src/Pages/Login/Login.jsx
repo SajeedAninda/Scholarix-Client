62b3784abc594cd6054667f0d3732137d0b44e70
SajeedAninda <sajaninda@gmail.com>
2024-01-09 20:29:47 +0600

    Added Lottie animation in login page

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import loginLottie from "../../assets/LottieFiles/loginLottie.json"
+import Lottie from 'lottie-react';
 
 const Login = () => {
     return (
         <div>
             <div className="min-h-screen text-gray-900 flex justify-center">
                 <div
-                    className="max-w-screen-xl m-0 sm:m-20 bg-white shadow sm:rounded-lg flex justify-center flex-1"
+                    className="w-[90%] mx-auto m-0 sm:m-20 bg-white flex justify-center"
                 >
-                    <div className="lg:w-1/2 xl:w-5/12 p-6 sm:p-12">
-                        <div className="mt-12 flex flex-col items-center">
+                    <div className="flex-1">
+                        <div className="mt-6 flex flex-col items-center">
                             <h1 className="text-2xl xl:text-3xl font-extrabold">
                                 Login to Scholarix
                             </h1>
@@ -103,15 +105,13 @@ const Login = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="flex-1 bg-indigo-100 text-center hidden lg:flex">
-                        <div
-                            className="m-12 xl:m-16 w-full bg-contain bg-center bg-no-repeat"
-                            style={{ backgroundImage: "url('https://storage.googleapis.com/devitary-image-host.appspot.com/15848031292911696601-undraw_designer_life_w96d.svg')" }}
-                        ></div>
+                    <div className="flex-1 text-center hidden lg:flex">
+                        <div>
+                            <Lottie animationData={loginLottie} loop={true} />
+                        </div>
                     </div>
                 </div>
             </div>
-
         </div>
     );
 };
