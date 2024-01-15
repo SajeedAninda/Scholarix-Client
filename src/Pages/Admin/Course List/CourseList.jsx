f894b7abffd84a57ce674b1499f1b28565529daa
SajeedAninda <sajaninda@gmail.com>
2024-01-15 14:35:45 +0600

    Mapped the courses

--- src/Pages/Admin/Course List/CourseList.jsx
+++ src/Pages/Admin/Course List/CourseList.jsx
@@ -23,23 +23,53 @@ const CourseList = () => {
 
             <div className='mt-4'>
                 <div className='bg-gradient-to-r from-[#ed4747] to-[#920707] rounded-tl-xl rounded-tr-xl grid grid-cols-11 px-6 py-4'>
+                    <div className='text-white font-bold text-xl col-span-2 text-center'>
+                        Image
+                    </div>
                     <div className='text-white font-bold text-xl col-span-3 text-center'>
                         Course
                     </div>
-                    <div className='text-white font-bold text-xl col-span-2 text-center'>
+                    <div className='text-white font-bold text-xl col-span-3 text-center'>
                         University
                     </div>
-                    <div className='text-white font-bold text-xl col-span-2 text-center'>
+                    <div className='text-white font-bold text-xl col-span-1 text-center'>
                         Degree
                     </div>
-                    <div className='text-white font-bold text-xl col-span-2 text-center'>
+                    <div className='text-white font-bold text-xl col-span-1 text-center'>
                         Update
                     </div>
-                    <div className='text-white font-bold text-xl col-span-2 text-center'>
+                    <div className='text-white font-bold text-xl col-span-1 text-center'>
                         Delete
                     </div>
                 </div>
             </div>
+
+            {
+                courses?.map(course =>
+                    <div className='mt-4'>
+                        <div className='bg-gradient-to-r from-[#ed4747] to-[#920707] rounded-tl-xl rounded-tr-xl grid grid-cols-11 px-6 py-4'>
+                            <div className='text-white font-bold text-xl col-span-2 text-center flex justify-center'>
+                                <img className='w-[50px] h-[50px] rounded-full' src={course?.imageUrl} alt="" />
+                            </div>
+                            <div className='text-white font-bold text-xl col-span-3 text-center'>
+                                {course?.course_name}
+                            </div>
+                            <div className='text-white font-bold text-xl col-span-3 text-center'>
+                                {course?.university_name}
+                            </div>
+                            <div className='text-white font-bold text-xl col-span-1 text-center'>
+                                {course?.degree_name}
+                            </div>
+                            <div className='text-white font-bold text-xl col-span-1 text-center'>
+                                Update
+                            </div>
+                            <div className='text-white font-bold text-xl col-span-1 text-center'>
+                                Delete
+                            </div>
+                        </div>
+                    </div>
+                )
+            }
         </div>
     );
 };
