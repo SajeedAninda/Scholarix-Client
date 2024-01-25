5d3f460912d18d5ef4aade330404dee59804c1a1
SajeedAninda <sajaninda@gmail.com>
2024-01-25 17:44:44 +0600

    Implemented Bookmark Deletion

--- src/Pages/User/Bookmarked Courses/BookmarkedCourses.jsx
+++ src/Pages/User/Bookmarked Courses/BookmarkedCourses.jsx
@@ -3,8 +3,11 @@ import useAxiosInstance from '../../../Hooks/useAxiosInstance';
 import { useQuery } from '@tanstack/react-query';
 import useAuth from '../../../Hooks/useAuth';
 import { Link } from 'react-router-dom';
-import CourseSkeleton from '../../../Components/Skeleton/CourseSkeleton';
 import TabCourseSkeleton from '../../../Components/Skeleton/TabCourseSkeleton';
+import { MdDelete } from "react-icons/md";
+import Swal from 'sweetalert2';
+import toast from 'react-hot-toast';
+
 
 const BookmarkedCourses = () => {
     let axiosInstance = useAxiosInstance();
@@ -19,6 +22,32 @@ const BookmarkedCourses = () => {
         }
     })
 
+    let handleDeleteBookmark = (id) => {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: 'Once Deleted, you cannot revert this!',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#0e2b45',
+            cancelButtonColor: '#ed4747',
+            confirmButtonText: 'Yes, Delete!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axiosInstance.delete(`/deleteBookmark/${id}`)
+                    .then(res => {
+                        if (res.data.deletedCount > 0) {
+                            refetch();
+                            console.log(res.data);
+                            toast.success("Bookmark Deleted Succesfully")
+                        }
+                    })
+                    .catch(error => {
+                        console.error("Error :", error);
+                        toast.error('Error', 'Failed to delete Bookmark');
+                    });
+            }
+        });
+    }
 
     return (
         <div className='w-[90%] mx-auto py-8'>
@@ -42,7 +71,13 @@ const BookmarkedCourses = () => {
                                 <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 py-6'>
                                     {bookmarkedCourses?.map(course => (
                                         <div key={course?._id} className="bg-whiterounded-lg shadow-lg flex flex-col">
-                                            <img className="rounded-t-lg w-full h-[200px] object-cover" src={course?.imageUrl} alt="" />
+                                            <div className='relative'>
+                                                <img className="rounded-t-lg w-full h-[200px] object-cover" src={course?.imageUrl} alt="" />
+
+                                                <button onClick={() => handleDeleteBookmark(course?._id)} className='bg-gradient-to-r from-[#920707] to-[#ed4747] p-3 rounded-full absolute top-2 right-2 cursor-pointer'>
+                                                    <MdDelete className='text-white text-3xl' />
+                                                </button>
+                                            </div>
 
 
                                             <div className="p-5 flex flex-col flex-grow bg-gradient-to-r from-[#920707] to-[#ed4747] rounded-b-lg">
@@ -50,7 +85,7 @@ const BookmarkedCourses = () => {
 
 
                                                 <p className="mb-3 font-normal text-white">
-                                                    {course?.university_name} in <span className='capitalize'>{course?.country_name}</span>
+                                                    {course?.university_name}
                                                 </p>
 
 
