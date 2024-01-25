6b68e7ab0610627eb23b3960046761e8791992af
SajeedAninda <sajaninda@gmail.com>
2024-01-25 15:28:02 +0600

    Designed User Dashboard

--- src/Pages/User/User Dashboard/UserDashboard.jsx
+++ src/Pages/User/User Dashboard/UserDashboard.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import userLottie from "../../../assets/LottieFiles/userLottie.json"
+import Lottie from 'lottie-react';
 
 const UserDashboard = () => {
     return (
-        <div>
-            
+        <div className='w-[90%] mx-auto'>
+            <div className='flex flex-col md:flex-row-reverse my-12 md:my-6 lg:my-2 justify-center items-center'>
+                <div className='flex-1 text-center'>
+                    <h1 className='text-7xl md:text-8xl lg:text-9xl font-bold'><span className='text-[#ed4747]'>User</span> <br /> <span className='text-[#0e2b45]'>Panel</span> <br /></h1>
+                    <h2 className='text-[#ed4747] text-5xl font-bold'>Take control of your Profile </h2>
+                </div>
+
+                <div className='flex-1'>
+                    <Lottie animationData={userLottie} loop={true} />
+                </div>
+            </div>
         </div>
     );
 };
