eb0bc9c268845935e4cb1b074f921e0f0761783e
SajeedAninda <sajaninda@gmail.com>
2024-02-01 16:28:46 +0600

    Created page links from success or failure page

--- src/Pages/Payment Result/Failed.jsx
+++ src/Pages/Payment Result/Failed.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Failed = () => {
     return (
@@ -6,7 +7,7 @@ const Failed = () => {
             <div className='bg-gradient-to-r from-[#ed4747] to-[#920707] p-8 rounded-lg shadow-2xl space-y-4'>
                 <h1 className='text-white font-bold text-4xl text-center'>Payment Failed.</h1>
                 
-                <h2 className='text-white font-bold text-2xl text-center'>Please try again to book consultant</h2>
+                <h2 className='text-white font-bold text-2xl text-center'>Please try again to book consultant from <Link className='underline hover:opacity-50' to={"/consultant"}>Consultant</Link> page</h2>
             </div>
         </div>
     );
--- src/Pages/Payment Result/Success.jsx
+++ src/Pages/Payment Result/Success.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 const Success = () => {
     let { tranId } = useParams();
@@ -8,7 +8,7 @@ const Success = () => {
             <div className='bg-gradient-to-r from-[#ed4747] to-[#920707] p-8 rounded-lg shadow-2xl space-y-4'>
                 <h1 className='text-white font-bold text-4xl text-center'>Your Payment is Successfull.</h1>
                 <h2 className='text-white font-bold text-2xl text-center bg-green-400 p-2 rounded-lg'>Payment ID:<span className='font-medium'> {tranId}</span></h2>
-                <h2 className='text-white font-bold text-2xl text-center'>Please check profile page to view more details</h2>
+                <h2 className='text-white font-bold text-2xl text-center'>Please check <Link className='underline hover:opacity-50' to={"/user"}>Profile</Link> page to view more details</h2>
             </div>
         </div>
     );
