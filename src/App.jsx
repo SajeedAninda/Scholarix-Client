2bcf98630cfa913b69fea50fee8b72ac5c9c5f0b
SajeedAninda <sajaninda@gmail.com>
2024-02-04 15:32:39 +0600

    Added scroll to top

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import './App.css'
 import Footer from './Components/Footer/Footer'
 import Navbar from './Components/Navbar/Navbar'
 import Homepage from './Pages/Homepage/Homepage'
+import ScrollToTop from './Components/Scroll/Scroll To Top/ScrollToTop'
 
 function App() {
 
   return (
     <>
+      <ScrollToTop></ScrollToTop>
       <Navbar></Navbar>
       <Outlet></Outlet>
       <Footer></Footer>
