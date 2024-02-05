9f36a2614f025be1a46d356b0372a261011c0c7e
SajeedAninda <sajaninda@gmail.com>
2024-02-05 15:52:18 +0600

    Made two more sections responsive

--- src/Components/How We Work/HowWeWork.jsx
+++ src/Components/How We Work/HowWeWork.jsx
@@ -16,46 +16,46 @@ const HowWeWork = () => {
                     <div class="border-2-2 absolute border-opacity-20 border-[#ed4747] h-full border" style={{ left: '50%' }}></div>
                     <div class="mb-8 flex justify-between items-center w-full right-timeline">
                         <div class="order-1 w-5/12"></div>
-                        <div class="z-20 flex items-center order-1 bg-gradient-to-r from-[#ed4747] to-[#920707] shadow-xl hover:shadow-[#ed4747] hover:shadow-2xl cursor-pointer w-8 h-8 rounded-full">
+                        <div class="hidden z-20 md:flex items-center order-1 bg-gradient-to-r from-[#ed4747] to-[#920707] shadow-xl hover:shadow-[#ed4747] hover:shadow-2xl cursor-pointer w-8 h-8 rounded-full">
                             <h1 class="mx-auto font-semibold text-lg text-[#F7FFF7]">1</h1>
                         </div>
-                        <div class="order-1 bg-gradient-to-r from-[#ed4747] to-[#920707] rounded-lg shadow-xl hover:shadow-[#ed4747] hover:shadow-2xl cursor-pointer w-5/12 px-6 py-4">
+                        <div class="order-1 bg-gradient-to-r from-[#ed4747] to-[#920707] rounded-lg shadow-xl hover:shadow-[#ed4747] hover:shadow-2xl cursor-pointer w-full md:w-5/12 px-6 py-4">
                             <h3 class="mb-3 font-bold text-[#F7FFF7] text-xl">Personalized Consultation</h3>
                             <p class="text-sm leading-snug tracking-wide text-[#F7FFF7] text-opacity-100">Initiate our journey with a one-on-one consultation, delving into your unique goals, preferences, and challenges to understand your educational aspirations.</p>
                         </div>
                     </div>
 
-                    
+
                     <div class="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
                         <div class="order-1 w-5/12"></div>
-                        <div class="z-20 flex items-center order-1 bg-gradient-to-r from-[#ed4747] to-[#920707] shadow-xl hover:shadow-[#ed4747] hover:shadow-2xl cursor-pointer w-8 h-8 rounded-full">
+                        <div class="hidden z-20 md:flex items-center order-1 bg-gradient-to-r from-[#ed4747] to-[#920707] shadow-xl hover:shadow-[#ed4747] hover:shadow-2xl cursor-pointer w-8 h-8 rounded-full">
                             <h1 class="mx-auto text-[#F7FFF7] font-semibold text-lg">2</h1>
                         </div>
-                        <div class="order-1 bg-gradient-to-r from-[#920707] to-[#ed4747] rounded-lg shadow-xl hover:shadow-[#ed4747] hover:shadow-2xl cursor-pointer w-5/12 px-6 py-4">
+                        <div class="order-1 bg-gradient-to-r from-[#920707] to-[#ed4747] rounded-lg shadow-xl hover:shadow-[#ed4747] hover:shadow-2xl cursor-pointer w-full md:w-5/12 px-6 py-4">
                             <h3 class="mb-3 font-bold text-[#F7FFF7] text-xl">Holistic Assessment</h3>
                             <p class="text-sm font-medium leading-snug tracking-wide text-[#F7FFF7] text-opacity-100">Conduct a comprehensive assessment to evaluate your academic background, skills, and ambitions, laying the foundation for a personalized and strategic approach.</p>
                         </div>
                     </div>
 
-                    
+
                     <div class="mb-8 flex justify-between items-center w-full right-timeline">
                         <div class="order-1 w-5/12"></div>
-                        <div class="z-20 flex items-center order-1 bg-gradient-to-r from-[#ed4747] to-[#920707] shadow-xl hover:shadow-[#ed4747] hover:shadow-2xl cursor-pointer w-8 h-8 rounded-full">
+                        <div class="hidden z-20 md:flex items-center order-1 bg-gradient-to-r from-[#ed4747] to-[#920707] shadow-xl hover:shadow-[#ed4747] hover:shadow-2xl cursor-pointer w-8 h-8 rounded-full">
                             <h1 class="mx-auto font-semibold text-lg text-[#F7FFF7]">3</h1>
                         </div>
-                        <div class="order-1 bg-gradient-to-r from-[#ed4747] to-[#920707] rounded-lg shadow-xl hover:shadow-[#ed4747] hover:shadow-2xl cursor-pointer w-5/12 px-6 py-4">
+                        <div class="order-1 bg-gradient-to-r from-[#ed4747] to-[#920707] rounded-lg shadow-xl hover:shadow-[#ed4747] hover:shadow-2xl cursor-pointer w-full md:w-5/12 px-6 py-4">
                             <h3 class="mb-3 font-bold text-[#F7FFF7] text-xl">Tailored Academic Planning</h3>
                             <p class="text-sm leading-snug tracking-wide text-[#F7FFF7] text-opacity-100">Craft a customized academic roadmap, outlining a trajectory that aligns seamlessly with your goals, ensuring a clear and achievable path to success..</p>
                         </div>
                     </div>
 
-                    
+
                     <div class="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
                         <div class="order-1 w-5/12"></div>
-                        <div class="z-20 flex items-center order-1 bg-gradient-to-r from-[#ed4747] to-[#920707] shadow-xl hover:shadow-[#ed4747] hover:shadow-2xl cursor-pointer w-8 h-8 rounded-full">
+                        <div class="hidden z-20 md:flex items-center order-1 bg-gradient-to-r from-[#ed4747] to-[#920707] shadow-xl hover:shadow-[#ed4747] hover:shadow-2xl cursor-pointer w-8 h-8 rounded-full">
                             <h1 class="mx-auto text-[#F7FFF7] font-semibold text-lg">4</h1>
                         </div>
-                        <div class="order-1 bg-gradient-to-r from-[#920707] to-[#ed4747] rounded-lg shadow-xl hover:shadow-[#ed4747] hover:shadow-2xl cursor-pointer w-5/12 px-6 py-4">
+                        <div class="order-1 bg-gradient-to-r from-[#920707] to-[#ed4747] rounded-lg shadow-xl hover:shadow-[#ed4747] hover:shadow-2xl cursor-pointer w-full md:w-5/12 px-6 py-4">
                             <h3 class="mb-3 font-bold text-[#F7FFF7] text-xl">Strategic Application Guidance</h3>
                             <p class="text-sm font-medium leading-snug tracking-wide text-[#F7FFF7] text-opacity-100">Collaborate on a meticulous strategy for university selection and application preparation, enhancing your chances of securing admission to the institutions that best suit your aspirations.</p>
                         </div>
@@ -63,46 +63,46 @@ const HowWeWork = () => {
 
                     <div class="mb-8 flex justify-between items-center w-full right-timeline">
                         <div class="order-1 w-5/12"></div>
-                        <div class="z-20 flex items-center order-1 bg-gradient-to-r from-[#ed4747] to-[#920707] shadow-xl hover:shadow-[#ed4747] hover:shadow-2xl cursor-pointer w-8 h-8 rounded-full">
+                        <div class="hidden z-20 md:flex items-center order-1 bg-gradient-to-r from-[#ed4747] to-[#920707] shadow-xl hover:shadow-[#ed4747] hover:shadow-2xl cursor-pointer w-8 h-8 rounded-full">
                             <h1 class="mx-auto font-semibold text-lg text-[#F7FFF7]">5</h1>
                         </div>
-                        <div class="order-1 bg-gradient-to-r from-[#ed4747] to-[#920707] rounded-lg shadow-xl hover:shadow-[#ed4747] hover:shadow-2xl cursor-pointer w-5/12 px-6 py-4">
+                        <div class="order-1 bg-gradient-to-r from-[#ed4747] to-[#920707] rounded-lg shadow-xl hover:shadow-[#ed4747] hover:shadow-2xl cursor-pointer w-full md:w-5/12 px-6 py-4">
                             <h3 class="mb-3 font-bold text-[#F7FFF7] text-xl">Test Preparation Support</h3>
                             <p class="text-sm leading-snug tracking-wide text-[#F7FFF7] text-opacity-100">Provide dedicated guidance and resources for standardized test preparation, ensuring you are well-equipped to excel in the necessary exams.</p>
                         </div>
                     </div>
 
-                    
+
                     <div class="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
                         <div class="order-1 w-5/12"></div>
-                        <div class="z-20 flex items-center order-1 bg-gradient-to-r from-[#ed4747] to-[#920707] shadow-xl hover:shadow-[#ed4747] hover:shadow-2xl cursor-pointer w-8 h-8 rounded-full">
+                        <div class="hidden z-20 md:flex items-center order-1 bg-gradient-to-r from-[#ed4747] to-[#920707] shadow-xl hover:shadow-[#ed4747] hover:shadow-2xl cursor-pointer w-8 h-8 rounded-full">
                             <h1 class="mx-auto text-[#F7FFF7] font-semibold text-lg">6</h1>
                         </div>
-                        <div class="order-1 bg-gradient-to-r from-[#920707] to-[#ed4747] rounded-lg shadow-xl hover:shadow-[#ed4747] hover:shadow-2xl cursor-pointer w-5/12 px-6 py-4">
+                        <div class="order-1 bg-gradient-to-r from-[#920707] to-[#ed4747] rounded-lg shadow-xl hover:shadow-[#ed4747] hover:shadow-2xl cursor-pointer w-full md:w-5/12 px-6 py-4">
                             <h3 class="mb-3 font-bold text-[#F7FFF7] text-xl">Application Submission Assistance</h3>
                             <p class="text-sm font-medium leading-snug tracking-wide text-[#F7FFF7] text-opacity-100">Offer support in preparing and submitting accurate applications, navigating deadlines, and presenting your credentials in the best possible light.</p>
                         </div>
                     </div>
 
-                    
+
                     <div class="mb-8 flex justify-between items-center w-full right-timeline">
                         <div class="order-1 w-5/12"></div>
-                        <div class="z-20 flex items-center order-1 bg-gradient-to-r from-[#ed4747] to-[#920707] shadow-xl hover:shadow-[#ed4747] hover:shadow-2xl cursor-pointer w-8 h-8 rounded-full">
+                        <div class="hidden z-20 md:flex items-center order-1 bg-gradient-to-r from-[#ed4747] to-[#920707] shadow-xl hover:shadow-[#ed4747] hover:shadow-2xl cursor-pointer w-8 h-8 rounded-full">
                             <h1 class="mx-auto font-semibold text-lg text-[#F7FFF7]">7</h1>
                         </div>
-                        <div class="order-1 bg-gradient-to-r from-[#ed4747] to-[#920707] rounded-lg shadow-xl hover:shadow-[#ed4747] hover:shadow-2xl cursor-pointer w-5/12 px-6 py-4">
+                        <div class="order-1 bg-gradient-to-r from-[#ed4747] to-[#920707] rounded-lg shadow-xl hover:shadow-[#ed4747] hover:shadow-2xl cursor-pointer w-full md:w-5/12 px-6 py-4">
                             <h3 class="mb-3 font-bold text-[#F7FFF7] text-xl">Interview Preparation</h3>
                             <p class="text-sm leading-snug tracking-wide text-[#F7FFF7] text-opacity-100">Prepare you for admission interviews, refining your communication skills and enhancing your ability to articulate your goals and aspirations effectively.</p>
                         </div>
                     </div>
 
-                    
+
                     <div class="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
                         <div class="order-1 w-5/12"></div>
-                        <div class="z-20 flex items-center order-1 bg-gradient-to-r from-[#ed4747] to-[#920707] shadow-xl hover:shadow-[#ed4747] hover:shadow-2xl cursor-pointer w-8 h-8 rounded-full">
+                        <div class="hidden z-20 md:flex items-center order-1 bg-gradient-to-r from-[#ed4747] to-[#920707] shadow-xl hover:shadow-[#ed4747] hover:shadow-2xl cursor-pointer w-8 h-8 rounded-full">
                             <h1 class="mx-auto text-[#F7FFF7] font-semibold text-lg">8</h1>
                         </div>
-                        <div class="order-1 bg-gradient-to-r from-[#920707] to-[#ed4747] rounded-lg shadow-xl hover:shadow-[#ed4747] hover:shadow-2xl cursor-pointer w-5/12 px-6 py-4">
+                        <div class="order-1 bg-gradient-to-r from-[#920707] to-[#ed4747] rounded-lg shadow-xl hover:shadow-[#ed4747] hover:shadow-2xl cursor-pointer w-full md:w-5/12 px-6 py-4">
                             <h3 class="mb-3 font-bold text-[#F7FFF7] text-xl">Post-Acceptance Support</h3>
                             <p class="text-sm font-medium leading-snug tracking-wide text-[#F7FFF7] text-opacity-100">Guide you through the post-acceptance phase, assisting with enrollment processes and addressing any concerns to ensure a smooth transition into your chosen academic institution..</p>
                         </div>
