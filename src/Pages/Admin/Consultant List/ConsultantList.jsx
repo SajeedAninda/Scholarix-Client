4f6504c19220161c01d7b44f0013a46248ce0e65
SajeedAninda <sajaninda@gmail.com>
2024-02-06 18:17:38 +0600

    Made course list and consultant list pages responsive

--- src/Pages/Admin/Consultant List/ConsultantList.jsx
+++ src/Pages/Admin/Consultant List/ConsultantList.jsx
@@ -40,33 +40,33 @@ const ConsultantList = () => {
     }
 
     return (
-        <div className='mx-auto w-[90%] py-8'>
+        <div className='mx-auto w-[98%] md:w-[90%] py-8'>
             <div className='space-y-2'>
                 <h1 className='text-[#0e2b45] text-4xl font-bold text-center'>Consultant List</h1>
-                <h2 className='text-[#0e2b45] text-xl font-bold text-center'>
+                <h2 className='text-[#0e2b45] text-[9px] md:text-base lg:text-xl font-bold text-center'>
                     See all the consultants that you have added, Update their details or Delete their profile
                 </h2>
             </div>
 
             <div>
                 <div className='mt-4'>
-                    <div className='bg-gradient-to-r from-[#ed4747] to-[#920707] rounded-tl-xl rounded-tr-xl grid grid-cols-11 px-6 py-4'>
-                        <div className='text-white font-bold text-xl col-span-1 text-center'>
+                    <div className='bg-gradient-to-r from-[#ed4747] to-[#920707] rounded-tl-xl rounded-tr-xl grid grid-cols-11 px-2 md:px-6 py-4'>
+                        <div className='text-white font-bold text-[9px] md:text-base lg:text-xl col-span-1 text-center'>
                             Image
                         </div>
-                        <div className='text-white font-bold text-xl col-span-3 text-center'>
+                        <div className='text-white font-bold text-[9px] md:text-base lg:text-xl col-span-3 text-center'>
                             Full Name
                         </div>
-                        <div className='text-white font-bold text-xl col-span-3 text-center'>
+                        <div className='text-white font-bold text-[9px] md:text-base lg:text-xl col-span-3 text-center'>
                             Email
                         </div>
-                        <div className='text-white font-bold text-xl col-span-2 text-center'>
+                        <div className='text-white font-bold text-[9px] md:text-base lg:text-xl col-span-2 text-center'>
                             Expertise
                         </div>
-                        <div className='text-white font-bold text-xl col-span-1 text-center'>
+                        <div className='text-white font-bold text-[9px] md:text-base lg:text-xl col-span-1 text-center'>
                             Update
                         </div>
-                        <div className='text-white font-bold text-xl col-span-1 text-center'>
+                        <div className='text-white font-bold text-[9px] md:text-base lg:text-xl col-span-1 text-center'>
                             Delete
                         </div>
                     </div>
@@ -77,26 +77,26 @@ const ConsultantList = () => {
                         (
                             consultants?.map(consultant =>
                                 <div className=''>
-                                    <div className='bg-[#F7FFF7] border-b-2 border-[#ed4747] grid grid-cols-11 px-6 py-4 items-center'>
-                                        <div className='text-[#0e2b45] font-bold text-lg col-span-1 text-center flex justify-center'>
+                                    <div className='bg-[#F7FFF7] border-b-2 border-[#ed4747] grid grid-cols-11 px-2 md:px-6 py-4 items-center'>
+                                        <div className='text-[#0e2b45] font-bold text-[9px] md:text-base lg:text-xl col-span-1 text-center flex justify-center'>
                                             <img className='w-[50px] h-[50px] rounded-full object-cover' src={consultant?.imageUrl} alt="" />
                                         </div>
-                                        <div className='text-[#0e2b45] font-bold text-lg col-span-3 text-center'>
+                                        <div className='text-[#0e2b45] font-bold text-[9px] md:text-base lg:text-xl col-span-3 text-center'>
                                             {consultant?.fullName}
                                         </div>
-                                        <div className='text-[#0e2b45] font-bold text-lg col-span-3 text-center'>
+                                        <div className='text-[#0e2b45] font-bold text-[9px] md:text-base lg:text-xl col-span-3 text-center'>
                                             {consultant?.email}
                                         </div>
-                                        <div className='text-[#0e2b45] font-bold text-lg col-span-2 text-center capitalize'>
+                                        <div className='text-[#0e2b45] font-bold text-[9px] md:text-base lg:text-xl col-span-2 text-center capitalize'>
                                             {consultant?.expertise}
                                         </div>
 
-                                        <Link to={`updateCourse/${consultant?._id}`} className='text-[#0e2b45] font-bold text-lg col-span-1 text-center flex justify-center'>
-                                            <MdEditSquare className='text-3xl cursor-pointer font-bold text-[#0e2b45]' />
+                                        <Link to={`updateCourse/${consultant?._id}`} className='text-[#0e2b45] font-bold text-[9px] md:text-base lg:text-xl col-span-1 text-center flex justify-center'>
+                                            <MdEditSquare className='text-[12px] md:text-base lg:text-3xl cursor-pointer font-bold text-[#0e2b45]' />
                                         </Link>
 
-                                        <div onClick={() => handleDeleteCourse(consultant?._id)} className='text-[#0e2b45] font-bold text-lg col-span-1 text-center flex justify-center'>
-                                            <RiDeleteBinFill className='text-3xl cursor-pointer font-bold text-[#ed4747]' />
+                                        <div onClick={() => handleDeleteCourse(consultant?._id)} className='text-[#0e2b45] font-bold text-[9px] md:text-base lg:text-xl col-span-1 text-center flex justify-center'>
+                                            <RiDeleteBinFill className='text-[12px] md:text-base lg:text-3xl cursor-pointer font-bold text-[#ed4747]' />
                                         </div>
                                     </div>
                                 </div>
--- src/Pages/Admin/Course List/CourseList.jsx
+++ src/Pages/Admin/Course List/CourseList.jsx
@@ -55,7 +55,7 @@ const CourseList = () => {
     }
 
     return (
-        <div className='mx-auto w-[90%] py-8'>
+        <div className='mx-auto w-[98%] md:w-[90%] py-8'>
             <div className='space-y-2'>
                 <h1 className='text-[#0e2b45] text-4xl font-bold text-center'>Course List</h1>
                 <h2 className='text-[#0e2b45] text-xl font-bold text-center'>
@@ -75,23 +75,23 @@ const CourseList = () => {
             </div>
 
             <div className='mt-4'>
-                <div className='bg-gradient-to-r from-[#ed4747] to-[#920707] rounded-tl-xl rounded-tr-xl grid grid-cols-11 px-6 py-4'>
-                    <div className='text-white font-bold text-xl col-span-1 text-center'>
+                <div className='bg-gradient-to-r from-[#ed4747] to-[#920707] rounded-tl-xl rounded-tr-xl grid grid-cols-11 px-2 md:px-6 py-4'>
+                    <div className='text-white font-bold text-[9px] md:text-base lg:text-xl col-span-1 text-center'>
                         Image
                     </div>
-                    <div className='text-white font-bold text-xl col-span-3 text-center'>
+                    <div className='text-white font-bold text-[9px] md:text-base lg:text-xl col-span-3 text-center'>
                         Course
                     </div>
-                    <div className='text-white font-bold text-xl col-span-4 text-center'>
+                    <div className='text-white font-bold text-[9px] md:text-base lg:text-xl col-span-4 text-center'>
                         University
                     </div>
-                    <div className='text-white font-bold text-xl col-span-1 text-center'>
+                    <div className='text-white font-bold text-[9px] md:text-base lg:text-xl col-span-1 text-center'>
                         Degree
                     </div>
-                    <div className='text-white font-bold text-xl col-span-1 text-center'>
+                    <div className='text-white font-bold text-[9px] md:text-base lg:text-xl col-span-1 text-center'>
                         Update
                     </div>
-                    <div className='text-white font-bold text-xl col-span-1 text-center'>
+                    <div className='text-white font-bold text-[9px] md:text-base lg:text-xl col-span-1 text-center'>
                         Delete
                     </div>
                 </div>
@@ -102,26 +102,26 @@ const CourseList = () => {
                     (
                         filteredCourses?.map(course =>
                             <div className=''>
-                                <div className='bg-[#F7FFF7] border-b-2 border-[#ed4747] grid grid-cols-11 px-6 py-4 items-center'>
-                                    <div className='text-[#0e2b45] font-bold text-lg col-span-1 text-center flex justify-center'>
+                                <div className='bg-[#F7FFF7] border-b-2 border-[#ed4747] grid grid-cols-11  px-2 md:px-6 py-4 items-center'>
+                                    <div className='text-[#0e2b45] font-bold  text-[9px] md:text-base lg:text-xl col-span-1 text-center flex justify-center'>
                                         <img className='w-[50px] h-[50px] rounded-full object-cover' src={course?.imageUrl} alt="" />
                                     </div>
-                                    <div className='text-[#0e2b45] font-bold text-lg col-span-3 text-center'>
+                                    <div className='text-[#0e2b45] font-bold  text-[9px] md:text-base lg:text-xl col-span-3 text-center'>
                                         {course?.course_name}
                                     </div>
-                                    <div className='text-[#0e2b45] font-bold text-lg col-span-4 text-center'>
+                                    <div className='text-[#0e2b45] font-bold  text-[9px] md:text-base lg:text-xl col-span-4 text-center'>
                                         {course?.university_name}
                                     </div>
-                                    <div className='text-[#0e2b45] font-bold text-lg col-span-1 text-center capitalize'>
+                                    <div className='text-[#0e2b45] font-bold  text-[9px] md:text-base lg:text-xl col-span-1 text-center capitalize'>
                                         {course?.degree_name}
                                     </div>
 
-                                    <Link to={`updateCourse/${course?._id}`} className='text-[#0e2b45] font-bold text-lg col-span-1 text-center flex justify-center'>
-                                        <MdEditSquare className='text-3xl cursor-pointer font-bold text-[#0e2b45]' />
+                                    <Link to={`updateCourse/${course?._id}`} className='text-[#0e2b45] font-bold  text-[9px] md:text-base lg:text-xl col-span-1 text-center flex justify-center'>
+                                        <MdEditSquare className='text-[12px] md:text-base lg:text-3xl cursor-pointer font-bold text-[#0e2b45]' />
                                     </Link>
 
-                                    <div onClick={() => handleDeleteCourse(course?._id)} className='text-[#0e2b45] font-bold text-lg col-span-1 text-center flex justify-center'>
-                                        <RiDeleteBinFill className='text-3xl cursor-pointer font-bold text-[#ed4747]' />
+                                    <div onClick={() => handleDeleteCourse(course?._id)} className='text-[#0e2b45] font-bold  text-[9px] md:text-base lg:text-xl col-span-1 text-center flex justify-center'>
+                                        <RiDeleteBinFill className='text-[12px] md:text-base lg:text-3xl cursor-pointer font-bold text-[#ed4747]' />
                                     </div>
                                 </div>
                             </div>
