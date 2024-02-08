7f3bf4277ad7ad5c7c9cab4c3b851938e07ef593
SajeedAninda <sajaninda@gmail.com>
2024-02-08 15:33:46 +0600

    Created a react flash screen

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,47 @@ import Footer from './Components/Footer/Footer'
 import Navbar from './Components/Navbar/Navbar'
 import Homepage from './Pages/Homepage/Homepage'
 import ScrollToTop from './Components/Scroll/Scroll To Top/ScrollToTop'
+import { useEffect, useState } from 'react'
+import { Hourglass } from 'react-loader-spinner'
 
 function App() {
 
+  let [loading, setLoading] = useState();
+
+  useEffect(() => {
+    setLoading(true);
+
+    setTimeout(() => {
+      setLoading(false)
+    }, 3500);
+
+  }, [])
+
   return (
     <>
-      <ScrollToTop></ScrollToTop>
-      <Navbar></Navbar>
-      <Outlet></Outlet>
-      <Footer></Footer>
+      {
+        loading ?
+          <div className='flex flex-col gap-3 justify-center min-h-screen items-center'>
+            <Hourglass
+              visible={true}
+              height="80"
+              width="80"
+              ariaLabel="hourglass-loading"
+              wrapperStyle={{}}
+              wrapperClass=""
+              colors={['#ed4747', '#0e2b45']}
+            />
+            <h1 className='text-3xl md:text-5xl lg:text-6xl font-bold text-[#ed4747]'>Welcome to Scholarix!</h1>
+          </div>
+          :
+          <div>
+            <ScrollToTop></ScrollToTop>
+            <Navbar></Navbar>
+            <Outlet></Outlet>
+            <Footer></Footer>
+          </div>
+      }
+
     </>
   )
 }
